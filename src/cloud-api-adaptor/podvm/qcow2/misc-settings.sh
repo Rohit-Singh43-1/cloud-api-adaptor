0a68e5900eb28e6aaa55be04faffb26962acee3a
Rohit.Singh43@ibm.com <Rohit.Singh43@ibm.com>
2026-03-10 17:55:02 +0530

    Version lock

--- src/cloud-api-adaptor/podvm/qcow2/misc-settings.sh
+++ src/cloud-api-adaptor/podvm/qcow2/misc-settings.sh
@@ -32,7 +32,12 @@ if [[ "$CLOUD_PROVIDER" != "docker" ]]; then
     if [ ! -x "$(command -v iptables)" ]; then
         case $PODVM_DISTRO in
         rhel)
-            dnf -q install iptables -y
+            # dnf -q install iptables -y
+	    dnf -q install iptables-nft -y && dnf install -y kernel-modules-$(uname -r) kernel-modules-extra-$(uname -r)
+	    echo "install plugin"
+            dnf install -y 'dnf-command(versionlock)'
+            dnf versionlock add kernel-$(uname -r)
+            dnf versionlock add kernel-modules-$(uname -r)
             ;;
         ubuntu)
             apt-get -qq update && apt-get -qq install iptables -y
